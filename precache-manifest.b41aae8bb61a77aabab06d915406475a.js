9065c004bb1e8e0869b9c2ff5a0b3431da404508
Nathan <31359612+neerb@users.noreply.github.com>
2020-01-28 00:23:53 -0600

    Updates

---
+++ precache-manifest.b41aae8bb61a77aabab06d915406475a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc18ebab76a9f4bca8de7e927822f9e0",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "c53ce26a34d4432ee9c2",
+    "url": "/pokedex-react/static/css/main.1f401077.chunk.css"
+  },
+  {
+    "revision": "e93d61a8c11ac59a4097",
+    "url": "/pokedex-react/static/js/2.cb26297e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pokedex-react/static/js/2.cb26297e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c53ce26a34d4432ee9c2",
+    "url": "/pokedex-react/static/js/main.0402d156.chunk.js"
+  },
+  {
+    "revision": "5faf343ea0036357fb3d",
+    "url": "/pokedex-react/static/js/runtime-main.9f02b66c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pokedex-react/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "7e1c8e6592ff7059eeff26961b71eab6",
+    "url": "/pokedex-react/static/media/pokeball-png.7e1c8e65.png"
+  },
+  {
+    "revision": "edad5b0ba87af45e443be9c596c66d71",
+    "url": "/pokedex-react/static/media/rotate.edad5b0b.gif"
+  }
+]);
