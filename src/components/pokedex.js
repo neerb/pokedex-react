a412f461f0391da3da43b71f00fe5ba758dae5a6
Nathan <31359612+neerb@users.noreply.github.com>
2020-01-29 19:10:22 -0600

    Fixed sorting issue
    - Now displays pokemon in correct order

--- src/components/pokedex.js
+++ src/components/pokedex.js
@@ -5,9 +5,30 @@ import PokemonInformation from "./pokemoninformation";
 import ErrorMessage from "./errormessage";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
+const insertionSort = array => {
+  const length = array.length;
+
+  for (let i = 1; i < length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j].id > key.id) {
+      array[j + 1] = array[j];
+      j = j - 1;
+    }
+    array[j + 1] = key;
+  }
+  return array;
+};
+
 class Pokedex extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      pokemonList: [],
+      pokemonDataList: [],
+      searchedPokemonDataList: []
+    };
 
     fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000")
       .then(res => res.json())
@@ -19,11 +40,12 @@ class Pokedex extends Component {
           fetch(pokemon.url)
             .then(res => res.json())
             .then(result => {
-              let newList = this.state.pokemonDataList;
+              let newList = insertionSort(this.state.pokemonDataList);
 
               if (result.sprites.front_default != null) {
                 newList.push({
                   name: result.name,
+                  id: result.id,
                   imageUrl: result.sprites.front_default,
                   allInformation: result
                 });
@@ -37,69 +59,26 @@ class Pokedex extends Component {
         });
       });
 
+    console.log(this.state.pokemonDataList);
+
     //console.log(this.pokemonDataList);
-
-    this.state = {
-      name: null,
-      error: null,
-      isLoaded: false,
-      pokemon: null,
-      pokemonHeight: null,
-      abilities: [],
-      moves: [],
-
-      pokemonList: [],
-      pokemonDataList: [],
-      searchedPokemonDataList: []
-    };
-
-    this.fetchPokemonData = this.fetchPokemonData.bind(this);
     this.returnMappedImages = this.returnMappedImages.bind(this);
     this.showPokemonResults = this.showPokemonResults.bind(this);
-  }
-
-  fetchPokemonData(e) {
-    if (e.keyCode === 13) {
-      console.log("Enter Pressed");
-
-      const name = document.getElementById("pokemon-search-bar").value;
-
-      console.log("https://pokeapi.co/api/v2/pokemon/" + name);
-
-      fetch("https://pokeapi.co/api/v2/pokemon/" + name)
-        .then(res => res.json())
-        .then(
-          result => {
-            console.log(result.height);
-
-            this.setState({
-              name: result.name,
-              idnum: result.id,
-              isLoaded: true,
-              pokemonHeight: result.height,
-              abilities: result.abilities,
-              moves: result.moves
-            });
-          },
-
-          error => {
-            this.setState({ isLoaded: true, error });
-          }
-        );
-    }
   }
 
   showPokemonResults() {
     const name = document.getElementById("pokemon-search-bar").value;
     let newSearchList = [];
 
-    this.state.pokemonDataList.map(pokemon => {
-      if (name === "") {
-        newSearchList = this.state.pokemonDataList;
-      } else if (pokemon.name.includes(name)) {
-        newSearchList.push(pokemon);
-      }
-    });
+    if (name === "") {
+      newSearchList = this.state.pokemonDataList;
+    } else {
+      this.state.pokemonDataList.map(pokemon => {
+        if (pokemon.name.includes(name)) {
+          newSearchList.push(pokemon);
+        }
+      });
+    }
 
     this.setState({ searchedPokemonDataList: newSearchList });
   }
@@ -120,23 +99,15 @@ class Pokedex extends Component {
     );
   }
 
-  updateSelectedPokemonInformation() {}
+  componentDidMount() {
+    let sortedList = insertionSort(this.state.pokemonDataList);
 
-  /*
-  getImageFromURL(pokemon) {
-    fetch(pokemon.url)
-      .then(res => res.json())
-      .then(result => {
-        let newList = this.state.pokemonDataList;
-        newList.push({
-          name: result.name,
-          imageUrl: result.sprites.front_default
-        });
+    this.setState({ pokemonDataList: sortedList });
 
-        this.setState({ pokemonDataList: newList });
-      });
+    console.log(this.state.pokemonDataList);
   }
-  */
+
+  updateSelectedPokemonInformation() {}
 
   render() {
     const {
@@ -161,7 +132,6 @@ class Pokedex extends Component {
               <input
                 className="pokemon-search"
                 type="text"
-                onKeyDown={this.fetchPokemonData}
                 onChange={this.showPokemonResults}
                 id="pokemon-search-bar"
                 placeholder="Search for a pokemon..."
